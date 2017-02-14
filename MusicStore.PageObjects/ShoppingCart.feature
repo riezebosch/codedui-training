7a3302d66c47a9cbceb5904f2e238a254daeb42d
Manuel Riezebosch <mriezebosch@gmail.com>
2017-02-14 15:06:09 +0100

    specflow demo

---
+++ MusicStore.PageObjects/ShoppingCart.feature
@@ -0,0 +1,31 @@
+﻿Feature: ShoppingCart
+	In order to avoid silly mistakes
+	As a math idiot
+	I want to be told the sum of two numbers
+
+@mytag
+Scenario: Add same album twice
+	Given I have an empty shopping cart
+	When I select 'Rock' from the Store submenu
+		And I select 'Led Zeppelin III' from the overview
+		And I add this item to the cart
+	When I select 'Rock' from the Store submenu
+		And I select 'Led Zeppelin III' from the overview
+		And I add this item to the cart
+	Then the number on the shopping cart should be 2
+
+Scenario: Add different album
+	Given I have an empty shopping cart
+	When I select 'Jazz' from the Store submenu
+		And I select 'Quiet Songs' from the overview
+		And I add this item to the cart
+	When I select 'Rock' from the Store submenu
+		And I select 'Led Zeppelin III' from the overview
+		And I add this item to the cart
+	Then the number on the shopping cart should be 2
+
+Scenario: Add same album twice short
+	Given I have an empty shopping cart
+	When I add 'Led Zeppelin III' from the 'Rock' albums
+		And I add 'Led Zeppelin III' from the 'Rock' albums
+	Then the number on the shopping cart should be 2
---
+++ MusicStore.PageObjects/ShoppingCartSteps.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using TechTalk.SpecFlow;
+
+namespace MusicStore.PageObjects
+{
+    [Binding]
+    public class ShoppingCartSteps
+    {
+        [Given(@"I have an empty shopping cart")]
+        public void GivenIHaveAnEmptyShoppingCart()
+        {
+            IWebDriver driver = new ChromeDriver();
+            driver.Url = "http://localhost:5000";
+
+            var home = new HomePage(driver);
+            ScenarioContext.Current.Set(home);
+        }
+        
+        [When(@"I select '(.*)' from the Store submenu")]
+        public void WhenISelectFromTheStoreSubmenu(string genre)
+        {
+            var home = ScenarioContext.Current.Get<HomePage>();
+            var albums = home.ClickStore().SelectGenre(genre);
+
+            ScenarioContext.Current.Set(albums);
+        }
+        
+        [When(@"I select '(.*)' from the overview")]
+        public void WhenISelectFromTheOverview(string title)
+        {
+            var albums = ScenarioContext.Current.Get<AlbumOverviewPage>();
+            var details = albums.SelectAlbum(title);
+
+            ScenarioContext.Current.Set(details);
+        }
+        
+        [When(@"I add this item to the cart")]
+        public void WhenIAddThisItemToTheCart()
+        {
+            var details = ScenarioContext.Current.Get<AlbumDetailsPage>();
+            var cart = details.AddToCart();
+
+            ScenarioContext.Current.Set(cart);
+        }
+        
+        [Then(@"the number on the shopping cart should be (.*)")]
+        public void ThenTheNumberOnTheShoppingCartShouldBe(int total)
+        {
+            var cart = ScenarioContext.Current.Get<ShoppingCart>();
+            Assert.AreEqual(total, cart.CartStatus());
+        }
+
+        [When(@"I add '(.*)' from the '(.*)' albums")]
+        public void WhenIAddFromTheAlbums(string title, string genre)
+        {
+            var home = ScenarioContext.Current.Get<HomePage>();
+            var cart = home
+                .ClickStore()
+                .SelectGenre(genre)
+                .SelectAlbum(title)
+                .AddToCart();
+
+            ScenarioContext.Current.Set(cart);
+        }
+
+    }
+}
